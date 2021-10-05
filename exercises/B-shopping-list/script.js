7adf5df697914c8f540884d3c45785064e6267e6
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-10-05 16:05:52 +0100

    B-shopping-list completed
    again not the shortest and most tidy code, but it gets the job done.

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,14 @@
-function shoppingList(arrayOfPeople) {
+function shoppingList(arrayOfShopping) {
   // Write your code here...
+  const content = document.querySelector("#content");
+  const unorderedList = document.createElement("ul");
+  content.appendChild(unorderedList);
+  for(let i = 0; i < arrayOfShopping.length; i++){
+    let listItem = document.createElement("li");
+    let listText = document.createTextNode(arrayOfShopping[i]);
+    listItem.appendChild(listText);
+    unorderedList.appendChild(listItem);
+  }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
