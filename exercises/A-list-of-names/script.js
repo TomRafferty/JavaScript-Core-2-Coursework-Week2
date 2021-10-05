7529654e0ad063eedba3683490aaa88c5beba121
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-10-05 15:52:45 +0100

    A-list-of-names completed
    not in the most elegant way

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,18 @@
 function listOfNames(arrayOfPeople) {
-  let content = document.querySelector("#content");
+  const content = document.querySelector("#content");
+  let names = arrayOfPeople.map((x) => x.name);
+  let jobs = arrayOfPeople.map((x) => x.job);
+  //this isn't the prettiest way but I need to move on
+  for(let i = 0; i < people.length; i++){
+    const header1 = document.createElement("h1");
+    const header2 = document.createElement("h2");
+    let textNames = document.createTextNode(names[i]);
+    let textJobs = document.createTextNode(jobs[i]);
+    header1.appendChild(textNames);
+    header2.appendChild(textJobs);
+    content.appendChild(header1);
+    content.appendChild(header2);
+  }
 }
 
 let people = [
