1f0d7afed969da05535bdbf9385d3c6a70ab90de
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-10-11 13:13:14 +0100

    C-reading-list completed
    the css isn't correct but the layout is close enough and the DOM appears to be correct :)

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,37 +1,61 @@
 function readingList(books) {
   // Write your code here...
 
-  //separating into parts
-  const bookTitles = books.map((x) => (x = title));
-  const bookAuthors = books.map((x) => (x = author));
-  const booksAlreadyRead = books.map((x) => (x = alreadyRead));
+  //the css isn't exactly right
+  //but the main jist of it is there
 
+  //separating into parts
+  const bookTitles = books.map((x) => x.title);
+  const bookAuthors = books.map((x) => x.author);
+  const booksAlreadyRead = books.map((x) => x.alreadyRead);
+  console.log(bookTitles);
   //target
   const content = document.querySelector("#content");
 
+  //create header
+  const headerEl = document.createElement("h1");
+  headerEl.innerHTML = "Books";
+  content.insertBefore(headerEl, content.firstChild);
+
   //make the list element
   const unorderedList = document.createElement("ul");
+  unorderedList.style.display = "flex";
+  unorderedList.style.flexFlow = "row wrap";
+  unorderedList.style.listStyle = "none";
   content.appendChild(unorderedList);
 
-  //make the books
-  for(let i = 0; i < books.length; i++){
-    //I think thi is a pretty solid start but I'm gonna take a break now
+  for (let i = 0; i < books.length; i++) {
+    //create a book list item
     const bookItem = document.createElement("li");
-    content.appendChild(bookItem);
 
-    const bookTitlePara = document.createElement()
-    bookItem.appendChild(bookTitlePara);
+    bookItem.style.padding = "2em";
+    bookItem.style.margin = "1em";
+    bookItem.style.maxWidth = "10em";
+    unorderedList.appendChild(bookItem);
 
-    const bookTitleText = document.createTextNode(bookTitles[i]);
-    bookItem.appendChild(bookTitleText);
+    //add p elements for title and author
+    const titleParaEl = document.createElement("p");
+    titleParaEl.innerHTML = bookTitles[i];
+    bookItem.appendChild(titleParaEl);
 
-    const bookAuthorPara = document.createElement("p");
-    bookItem.appendChild(bookAuthorPara);
+    const authorParaEl = document.createElement("p");
+    authorParaEl.innerHTML = bookAuthors[i];
+    bookItem.appendChild(authorParaEl);
 
-    const bookAuthorText = document.createTextNode(bookAuthors[i]);
-    bookAuthorPara.appendChild(bookAuthorText);
+    //add img
+    const coverImgEl = document.createElement("img");
+    coverImgEl.src = books[i].coverUrl;
+    coverImgEl.style.height = "10em";
+    coverImgEl.style.width = "5em%";
+    bookItem.appendChild(coverImgEl);
+
+    //change back ground colour for each book
+    if (booksAlreadyRead[i] === true) {
+      bookItem.style.background = "green";
+    } else {
+      bookItem.style.background = "red";
+    }
   }
-  
 }
 
 const books = [
@@ -39,16 +63,22 @@ const books = [
     title: "The Design of Everyday Things",
     author: "Don Norman",
     alreadyRead: false,
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/81zpLhP1gWL.jpg",
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
     alreadyRead: true,
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71HMyqG6MRL.jpg",
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
     alreadyRead: true,
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
   },
 ];
 
