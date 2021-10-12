7500c849487c6af8ee886d6fa4d78ecc8614bed0
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-10-12 16:41:52 +0100

    D-to-do list started
    so far it'll let you check of items but wont let you uncheck them and I can't figure out why.

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,43 @@
 function todoList(todos) {
   // Write your code here...
+  const contentEl = document.getElementById("content");
+
+  //create the list and its elements
+  const unorderedListEL = document.createElement("ul");
+  contentEl.appendChild(unorderedListEL);
+  for (let i = 0; i < todos.length; i++) {
+    const listItemEl = document.createElement("li");
+    listItemEl.innerHTML = todos[i].todo;
+    unorderedListEL.appendChild(listItemEl);
+
+    const listItemObject = todos[i];
+
+    function lineThrough() {
+      listItemEl.style.textDecoration = "line-through";
+      listItemObject.isChecked = true;
+      console.log("wasn't checked, is now checked.");
+      console.log(listItemObject.isChecked);
+    }
+    function removeLine() {
+      listItemEl.style.textDecoration = "none";
+      listItemObject.isChecked = false;
+      console.log("was checked, not anymore");
+      console.log(listItemObject.isChecked);
+    }
+    if (listItemObject.isChecked === false) {
+      listItemEl.addEventListener("click", lineThrough);
+    }
+    if (listItemObject.isChecked === true) {
+      listItemEl.addEventListener("click", removeLine);
+    }
+  }
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes", isChecked: false },
+  { todo: "walk the dog", isChecked: false },
+  { todo: "learn javascript", isChecked: false },
+  { todo: "go shopping", isChecked: false },
 ];
 
 todoList(todos);
